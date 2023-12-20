ad3a4936789f7304a25095c8fa4669874088157a
Tamias <n.tamia5@icloud.com>
2023-12-20 19:38:58 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,34 +1,26 @@
 import React, { Component } from 'react';
 import { GalletyItem } from './ImageGalleryItem.styled';
 import { ModalImg } from 'components/Modal/Modal';
+import { useState } from 'react';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
+export const ImageGalleryItem = ({ webformatURL, largeImageURL, tags }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleImageClick = () => {
+    setIsModalOpen(prevIsModalOpen => !prevIsModalOpen);
   };
 
-  handleImageClick = () => {
-    this.setState(prevState => ({
-      isModalOpen: !prevState.isModalOpen,
-    }));
-  };
-
-  render() {
-    const { webformatURL, largeImageURL, tags } = this.props;
-    const { isModalOpen } = this.state;
-
-    return (
-      <GalletyItem data-img={largeImageURL} onClick={this.handleImageClick}>
-        <img src={webformatURL} alt={tags} />
-        {isModalOpen && (
-          <ModalImg
-            imageUrl={largeImageURL}
-            tags={tags}
-            isOpen={isModalOpen}
-            onClose={this.handleImageClick}
-          />
-        )}
-      </GalletyItem>
-    );
-  }
-}
+  return (
+    <GalletyItem data-img={largeImageURL} onClick={handleImageClick}>
+      <img src={webformatURL} alt={tags} />
+      {isModalOpen && (
+        <ModalImg
+          imageUrl={largeImageURL}
+          tags={tags}
+          isOpen={isModalOpen}
+          onClose={handleImageClick}
+        />
+      )}
+    </GalletyItem>
+  );
+};
